3674fd534658bafdc1c86901170d202bb668d837
vuttgl123 <phamtuanvu1401@gmail.com>
2025-07-09 22:49:56 +0700

    Commit lan dau

---
+++ backend/Controllers/DbTestController.cs
@@ -0,0 +1,31 @@
+﻿using backend.Persistence.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DbTestController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DbTestController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult TestDbConnection()
+        {
+            try
+            {
+                bool canConnect = _context.Database.CanConnect();
+                return Ok(new { canConnect });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { error = ex.Message });
+            }
+        }
+    }
+}
---
+++ backend/Program.cs
@@ -0,0 +1,48 @@
+using backend.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins(
+            "http://localhost:5173",
+            "https://xekhach.click"
+        )
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection")));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors("AllowFrontend");
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapControllers();
+
+app.Run();
